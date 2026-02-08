e86918cfed52377f6273beba1c7165930aea1bec
Ethan Vaiz <edanvaiz@gmail.com>
2026-02-08 00:43:49 -0600

    Implemented Google Vision and Configs
    Replaced TrOCR model due to errors caused by discrepancies in the transformers and torch dependencies. Instead google's multimodal gemini 2.0 Flash was used to extract text for the performance version. A configurations section was added for users to directly edit allowed characters, context and the selected mode (Lite/Performance). Includes a couple bug fixes in session state.

--- DigiScribe.py
+++ DigiScribe.py
@@ -5,46 +5,84 @@ import streamlit as st
 from google import genai
 from google.genai import types
 import numpy as np
-from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 from PIL import Image
-import torch
+import time
 # import pyperclip
 
+if "MODE" not in st.session_state:
+    st.session_state.MODE = "Lite"
+    st.session_state.context = ""
+    st.session_state.context_sentence = ""
+    st.session_state.allowlist = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,!?$ "
 
 handwriting_reader = easyocr.Reader(['en'], gpu = False, verbose=True)
-processor = TrOCRProcessor.from_pretrained('microsoft/trocr-small-handwritten')
-model = VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-small-handwritten')
+client = genai.Client(api_key = st.secrets["API_KEY"])
+if "uploaded" not in st.session_state:  
+    st.session_state["uploaded"] = False
+
+@st.dialog("**Configurations**", on_dismiss = "rerun")
+def configurations():
+    st.pills("**Mode:**", options = ["Lite", "Performance"], selection_mode = "single", key = "MODE_input")
+
+    st.text_input("Provide context/topic for image:", placeholder = "Enter context", key = "context_input")
+
+    st.write(f"Current allowed list of characters: [space]{st.session_state.allowlist}")
+    st.text_input("Enter allowed list of characters", key = "allowlist")
+
+    def submit():
+        if st.session_state.MODE_input:
+            st.session_state.MODE = st.session_state.MODE_input
+        if st.session_state.context_input.strip() != "":
+            st.session_state.context_sentence = "The context for the image: \"" + st.session_state.context_input + "\"."
+        if " " not in st.session_state.allowlist:
+            st.session_state.allowlist = st.session_state.allowlist + " "
+
+
+
+    submit_container = st.container(horizontal_alignment = "right")
+    submit_container.button("Confirm", type = "primary", on_click = submit)
+
+    
+
+
+
+
+
 
 def recognize(image):
-    # Read bytes from Streamlit file
-    file_bytes = np.frombuffer(image.read(), np.uint8)
-    # Decode into OpenCV image
-    image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    time.sleep(2)
 
-    # Sharpen edges in image to enhance accuracy:
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # Turn image into grayscale
-    sharpener = numpy.array([[-1,-1,-1],[-1,9,-1],[-1,-1,-1]]) # Define Filter Kernel
-    sharpen = cv2.filter2D(gray, -1, sharpener) # Apply Filter Kernel
-    thresh = cv2.threshold(sharpen, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-
-    pixel_values = processor(images=thresh, return_tensors="pt").pixel_values
-    outputs = model.generate(pixel_values, return_dict_in_generate=True, output_scores=True, num_beams=5)
-    generated_text = processor.batch_decode(outputs.sequences, skip_special_tokens=True)[0]
-    confidence = torch.exp(outputs.sequences_scores).item()
-
-    response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents = f"This text was taken out of an OCR software. Refine the words, phrases, or sentences that are nonsensical so that the final text is intelligible. Only output the final, refined text. Add punctuation accordingly. Here is the input text: {generated_text}",
-    config = types.GenerateContentConfig(
-        temperature = 0.1 # Using a Lower temperature since the task does not necessitate variety 
+    image.seek(0)
+    image_bytes = image.read()
+    try:
+        vision_text = client.models.generate_content(
+            model = "gemini-2.0-flash",
+            contents = [
+                types.Part.from_bytes(data = image_bytes, mime_type = "image/jpeg"),
+                "Transcribe the text from the image exactly. Do not perform any spelling/grammar/context changes keep the exact layout as it was given."
+            ]
         )
-        )
+    except:
+        st.error("Too many server requests. Try again later.")
+        st.stop()
+        return
 
-    return generated_text, response.text, confidence
+    try:
+        response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents = f"This text was taken out of an OCR software. Refine the words, phrases, or sentences that are nonsensical so that the final text is intelligible. Do not change the order of the original characters after refinement. First fix spelling issues then move onto grammar issues. Only output the final, refined text. Add punctuation accordingly. {st.session_state.context_sentence} Here is the input text: {vision_text}",
+        config = types.GenerateContentConfig(
+            temperature = 0.1 # Using a Lower temperature since the task does not necessitate variety 
+            )
+            )
+    except:
+        st.error("Too many server requests. Try again later.")
+        st.stop()
+        return
 
-client = genai.Client(api_key = st.secrets["API_KEY"])
 
-st.session_state["uploaded"] = False
+    return vision_text, response.text
+
 
 def extract_text(file_param):
     # Read bytes from Streamlit file
@@ -54,13 +92,13 @@ def extract_text(file_param):
 
     # Sharpen edges in image to enhance accuracy:
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # Turn image into grayscale
-    sharpener = numpy.array([[-1,-1,-1],[-1,9,-1],[-1,-1,-1]]) # Define Filter Kernel
+    sharpener = np.array([[-1,-1,-1],[-1,9,-1],[-1,-1,-1]]) # Define Filter Kernel
     sharpen = cv2.filter2D(gray, -1, sharpener) # Apply Filter Kernel
     thresh = cv2.threshold(sharpen, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
 
-    text = handwriting_reader.readtext(thresh, detail = 1, allowlist = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,!?$ ")
-    simple_text = str(" ".join(handwriting_reader.readtext(thresh, detail = 0, allowlist = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,!?$ ", paragraph = True)))
+    text = handwriting_reader.readtext(thresh, detail = 1, allowlist = st.session_state.allowlist)
+    simple_text = str(" ".join(handwriting_reader.readtext(thresh, detail = 0, allowlist = st.session_state.allowlist, paragraph = True)))
     # simple_text = simple_text[2:-2]
 
     bbox_list = []
@@ -83,26 +121,34 @@ def extract_text(file_param):
         annotated_text += "***" + str(segment) + "***"
         annotated_text += " (Confidence: " +  str(confidence_list[i]) + "), "
 
-
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents = f"This text was taken out of an OCR software. Refine the words, phrases, or sentences that are nonsensical so that the final text is intelligible. Only output the final, refined text. Add punctuation accordingly. The confidence scores from the OCR model are also given, for high confidence segments avoid changing it unless there are clarity/spelling/context issues. Here is the input text: {annotated_text}",
-        config = types.GenerateContentConfig(
-            temperature = 0.1 # Using a Lower temperature since the task does not necessitate variety 
+    try: 
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents = f"This text was taken out of an OCR software. Refine the words, phrases, or sentences that are nonsensical so that the final text is intelligible. Do not change the order of the original characters after refinement. First fix spelling issues then move onto grammar issues. Only output the final, refined text. Add punctuation accordingly. The confidence scores from the OCR model are also given, for high confidence segments avoid changing it unless there are clarity/spelling/context issues. {st.session_state.context_sentence} Here is the input text: {annotated_text}",
+            config = types.GenerateContentConfig(
+                temperature = 0.1 # Using a Lower temperature since the task does not necessitate variety 
+            )
         )
-    )
+    except:
+        st.error("Too many server requests. Try again later.")
+        st.stop()
+        return
 
     return simple_text, response.text, annotated_text, average_confidence
 
+title, config = st.columns([0.93, 0.07])
+if st.session_state.MODE == "Lite":
+    title.title("***Digi:blue[Scribe]***   :yellow[  Lite]")
+elif st.session_state.MODE == "Performance":
+    title.title("***Digi:blue[Scribe]***   :red[  Performance]")
 
-st.title("***Digi:blue[Scribe]***")
+config.button("", icon = ":material/settings:", on_click = configurations)
 
 upload, cam = st.columns([0.5,0.5])
 
 with upload:
     uploaded_file = st.file_uploader(label = "Upload an Image for Conversion (PNG, JPG, JPEG)", type = ["jpg", "jpeg", "png"])  # r"Handwriting Recognition\Images_Examples\aTfamilymovingsentence.png"
-    with st.container(border = True, height = 90):
-        MODE = st.pills("**Mode:**", options = ["Lite", "Performance"], selection_mode = "single")
+
 
 with cam:
     captured_file = st.camera_input("Take a picture", )
@@ -128,16 +174,17 @@ if st.session_state["uploaded"]:
     text = "" 
     extra_details = ""
     with st.spinner("Extracting...", show_time = True):
-        if MODE == "Lite":
+        if st.session_state.MODE == "Lite":
             text, refined_text, extra_details, avg = extract_text(FILE)
-        elif MODE == "Performance":
-            text, refined_text, avg = recognize(FILE)
+        elif st.session_state.MODE == "Performance":
+            text, refined_text= recognize(FILE)
 
 
 extracted, refined, img = st.tabs(["Initially Extracted Text","Refined Text", "Image"])
 
 with extracted:
-    st.text_area(label = " ", placeholder = text, disabled = True, key = "extr")
+    ex_cont = st.container(height = 150, key = "extc")
+    ex_cont.write(text)
     # extracted_copy, extracted_download = st.columns([0.05,0.95])
     # with extracted_copy:
     #     if st.button(label = "", icon=":material/content_copy:", type = "tertiary", key = 'ec'):
@@ -146,7 +193,8 @@ with extracted:
     st.download_button("Download Extracted Text", data = text, file_name = "digi_scribe_extracted_text.txt", icon=":material/download:", on_click = "ignore")
 
 with refined:
-    st.text_area(label = " ",placeholder = refined_text, disabled = True, key = "ref")
+    ref_cont = st.container(height = 150, key = "refc")
+    ref_cont.write(refined_text)
     # refined_copy, refined_download = st.columns([0.05,0.95])
     # with refined_copy:
     #     if st.button(label = "", icon=":material/content_copy:", type = "tertiary", key = 'rc'):
@@ -159,12 +207,14 @@ with img:
         st.image(FILE)
 
 
-if st.session_state["uploaded"]:
+if st.session_state["uploaded"] and st.session_state.MODE == "Lite":
     st.divider()
     with st.expander("Extra Data/Stats:"):
         st.write(f"***Average Confidence:*** {avg}")
-        if MODE == "Lite":
-            st.write(extra_details)
+        st.write(extra_details)
+        st.write("***Configurations***")
+        st.write(st.session_state.context_sentence)
+        st.write("Allowed characters: [space]" + st.session_state.allowlist)
 
 
 
@@ -174,7 +224,7 @@ if st.session_state["uploaded"]:
 # TODO: Segment text before recognition
 
 
-##### Later #####
+#---------- Later ----------#
 # TODO: Change font to Indie Flower from google fonts by using tutorial: https://docs.streamlit.io/develop/tutorials/configuration-and-theming/external-fonts 
 # TODO: Make third tab with Image with Bounded Box of words
 # TODO: Improve speed with pytorch threads
