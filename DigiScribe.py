93dd766a41b8dde072ed4d7bb2720d449f8cb1b8
Ethan Vaiz <edanvaiz@gmail.com>
2026-02-18 23:28:58 -0600

    Fixing Element Styling #12

--- DigiScribe.py
+++ DigiScribe.py
@@ -185,7 +185,7 @@ with st.container(border = True):
 
     with upload:
         uploaded_file = st.file_uploader(label = "**Upload an Image for Conversion (PNG, JPG, JPEG)**", type = ["jpg", "jpeg", "png"])  # r"Handwriting Recognition\Images_Examples\aTfamilymovingsentence.png"
-        st.space(148)
+        st.space(200)
     with cam:
         captured_file = st.camera_input("**Take a picture**")
 
