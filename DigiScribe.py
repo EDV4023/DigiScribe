6969367655f29511f68220964c69c8f034bdef18
Ethan Vaiz <edanvaiz@gmail.com>
2026-02-23 16:01:26 -0600

    Added Custom Sidebar

--- DigiScribe.py
+++ DigiScribe.py
@@ -13,6 +13,18 @@ import time
 st.set_page_config(page_title = "DigiScribe")
 st.logo(image = r"DigiScribe_Logo.png", icon_image = r"DigiScribe_logo_icon.png")
 
+# Sidebar customized menu
+st.sidebar.markdown("**Extract Text from Images:**")
+st.sidebar.page_link(r"DigiScribe.py", label = "DigiScribe", icon = ":material/image:")
+
+st.sidebar.markdown("**Markdown Text Editor:**")
+st.sidebar.page_link(r"pages/TextEditor.py", label = "Text Editor", icon = ":material/edit:")
+
+st.sidebar.markdown("**Student Hub:**")
+st.sidebar.page_link(r"pages/TextEditor.py", label = "Student Hub", icon = ":material/school:")
+
+
+
 st.session_state.initial_run = False
 
 if "MODE" not in st.session_state:
