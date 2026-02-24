727eb71d6dcb48b12bcd50122b30ca629abeed7e
Ethan Vaiz <edanvaiz@gmail.com>
2026-02-23 18:42:10 -0600

    Menu Styling Changes

--- pages/StudentHub.py
+++ pages/StudentHub.py
@@ -7,11 +7,7 @@ st.logo(image = r"DigiScribe_Logo.png",icon_image = r"DigiScribe_logo_icon.png",
 
 # Sidebar customized menu
 st.sidebar.title("**DigiScribe Menu**")
-st.sidebar.markdown("""
-<style> h1 {font-size:25px} </style>
-"""
-            ,unsafe_allow_html = True)
-st.sidebar.space("xxsmall")
+st.sidebar.space(5)
 
 st.sidebar.subheader("**Extract Text from Images:**")
 st.sidebar.page_link(r"DigiScribe.py", label = "*DigiScribe*", icon = ":material/image:")
