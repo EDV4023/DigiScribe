292e80b1137a696a86f98308f0828427666044a0
Ethan Vaiz <edanvaiz@gmail.com>
2026-02-23 18:40:47 -0600

    Experimental CSS #2

--- pages/TextEditor.py
+++ pages/TextEditor.py
@@ -8,7 +8,7 @@ st.logo(image = r"DigiScribe_Logo.png",icon_image = r"DigiScribe_logo_icon.png",
 # Sidebar customized menu
 st.sidebar.title("**DigiScribe Menu**")
 st.sidebar.markdown("""
-<style> h1 {font-size:25px} </style>
+<style> h1 {font-size:50px} </style>
 """
             ,unsafe_allow_html = True)
 st.sidebar.space("xxsmall")
